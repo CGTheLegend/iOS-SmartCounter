2a413ff1e64bf72a679c475858ba6f3f30b285a1
Cristian Gonzalez <cfgonzal@ucsc.edu>
2014-08-26 14:04:28 -0700

    Adding the class for the custom UIButton. Not yet completed.

---
+++ SmartCounter/SmartCounterRoundRecButton.h
@@ -0,0 +1,13 @@
+//
+//  SmartCounterRoundRecButton.h
+//  SmartCounter
+//
+//  Created by Cristian Gonzalez on 8/26/14.
+//  Copyright (c) 2014 Cristian F. Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmartCounterRoundRecButton : UIButton
+
+@end
---
+++ SmartCounter/SmartCounterRoundRecButton.m
@@ -0,0 +1,33 @@
+//
+//  SmartCounterRoundRecButton.m
+//  SmartCounter
+//
+//  Created by Cristian Gonzalez on 8/26/14.
+//  Copyright (c) 2014 Cristian F. Gonzalez. All rights reserved.
+//
+
+#import "SmartCounterRoundRecButton.h"
+
+@implementation SmartCounterRoundRecButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+//    [self.layer setCornerRadius:10.0f];
+//    [self.layer setBorderColor:[UIColor blackColor].CGColor];
+//    [self.layer setBorderWidth:1.0];
+}
+
+
+@end
