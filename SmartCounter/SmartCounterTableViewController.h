d5b61fe54cec904e2798ab3623be4f2b1d29287f
Cristian Gonzalez <cfgonzal@ucsc.edu>
2014-09-09 09:29:52 -0700

    Added Table View Controller base files.

---
+++ SmartCounter/SmartCounterTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SmartCounterTableViewController.h
+//  SmartCounter
+//
+//  Created by Cristian Gonzalez on 9/9/14.
+//  Copyright (c) 2014 Cristian F. Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmartCounterTableViewController : UITableViewController
+
+@property NSMutableArray *listItems;
+
+@end
